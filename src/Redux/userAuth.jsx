2fac17641f35626ee9a8f72ecfe12a40f88c6e2c
aswanth <aswanthr18tech@gmail.com>
2024-05-08 12:14:11 +0530

    token value changed from null to token

--- src/Redux/userAuth.jsx
+++ src/Redux/userAuth.jsx
@@ -3,7 +3,7 @@ const token="fs1234"
 export const UserAuth = createSlice({
     name: "client",
     initialState: {
-        Token:null, // Get token from localStorage or set default
+        Token:token, // Get token from localStorage or set default
     },
     reducers: {
 
